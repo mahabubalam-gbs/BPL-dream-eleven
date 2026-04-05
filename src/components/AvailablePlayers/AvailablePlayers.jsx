5edaa66990a9b25b1075802c4d65351290622d80
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-04-06 00:34:35 +0600

    Partial card design done

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,11 +1,34 @@
 import React, { use } from 'react';
+import userImg from "../../assets/user-1.png"
+import flagImg from "../../assets/report-1.png"
 
 const AvailablePlayers = ({ playersPromise }) => {
     const playersData = use(playersPromise)
     console.log(playersData)
     return (
-        <div>
-
+        <div className='max-w-300 mx-auto'>
+            <div className="card bg-base-100 w-96 shadow-sm p-4">
+                <figure>
+                    <img className='rounded-xl'
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                </figure>
+                <div className="mt-4">
+                    <div className='flex items-center'>
+                        <img className='w-5 h-5' src={userImg} alt="" /> <h2 className="card-title ml-2">Viral Kohli</h2>
+                    </div>
+                    <div className='flex items-center justify-between mt-2 pb-4 border-b border-gray-200'>
+                        <div className='flex items-center'>
+                            <img className='w-4 h-4 mr-2' src={flagImg} alt="" />
+                            <span>Bangladesh</span>
+                        </div>
+                        <button className='btn btn-sm'>All-Rounder</button>
+                    </div>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary mt-4">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <div className="flex-1">
                     <img className="w-15 h-15" src={navImg} alt="" />
                 </div>
-                <div className="flex items-center border p-1">
+                <div className="flex items-center border border-gray-200 rounded-lg p-2 font-bold">
                     <span className='mr-1'>600000000</span>
                     <span className='mr-1'>Coin</span>
                     <img src={dollarImg} alt="" />
