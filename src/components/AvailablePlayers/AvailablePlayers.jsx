9c982705a291933fe41348ccad27165ff2b29d29
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-04-06 14:58:51 +0600

    Responsive card design done

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -7,7 +7,7 @@ const AvailablePlayers = ({ playersPromise }) => {
     const playersData = use(playersPromise)
 
     return (
-        <div className='max-w-300 mx-auto'>
+        <div className='max-w-300 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
             {
                 playersData.map(player => <Players key={player.id} player={player}></Players>
                 )
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -3,37 +3,40 @@ import userImg from "../../assets/user-1.png"
 import flagImg from "../../assets/report-1.png"
 
 const Players = ({ player }) => {
-    console.log(player)
+
+    const { player_image, player_name, player_country, bating_style, bowling_style, playing_role, price, rating } = player;
+
     return (
         <div>
-            < div className="card bg-base-100 w-96 shadow-sm p-4" >
+            < div className="card bg-base-100 shadow-sm p-4" >
                 <figure>
-                    <img className='rounded-xl'
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    <img className='rounded-xl w-full h-70 object-cover'
+                        src={player_image}
                         alt="Shoes" />
                 </figure>
                 <div className="mt-4">
                     <div className='flex items-center'>
-                        <img className='w-5 h-5' src={userImg} alt="" /> <h2 className="card-title ml-2 font-bold">Viral Kohli</h2>
+                        <img className='w-5 h-5' src={userImg} alt="" />
+                        <h2 className="card-title ml-2 font-bold">{player_name}</h2>
                     </div>
                     <div className='flex items-center justify-between mt-2 pb-4 border-b border-gray-200'>
                         <div className='flex items-center'>
                             <img className='w-4 h-4 mr-2' src={flagImg} alt="" />
-                            <span>Bangladesh</span>
+                            <span>{player_country}</span>
                         </div>
-                        <button className='btn btn-sm'>All-Rounder</button>
+                        <button className='btn btn-sm'>{playing_role}</button>
                     </div>
 
                     <div className='flex justify-between font-bold pt-3'>
                         <span>Rating</span>
-                        <span>5</span>
+                        <span>{rating}</span>
                     </div>
                     <div className='flex justify-between  pt-2'>
-                        <span className='font-semibold'>Left hand bat</span>
-                        <span className='text-gray-500'>Right hand bat</span>
+                        <span className='font-semibold'>{bating_style}</span>
+                        <span className='text-gray-500'>{bowling_style}</span>
                     </div>
                     <div className="card-actions flex justify-between items-center mt-2">
-                        <p className='font-semibold'>Price: $150000</p>
+                        <p className='font-semibold'>Price: ${price}</p>
                         <button className="btn ">Choose Player</button>
                     </div>
                 </div>
