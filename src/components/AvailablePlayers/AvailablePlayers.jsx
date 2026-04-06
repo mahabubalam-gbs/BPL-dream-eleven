c578f486fdf646f86bad9b80725272aacb7ab379
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-04-06 11:59:08 +0600

    Basic card design done

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -15,7 +15,7 @@ const AvailablePlayers = ({ playersPromise }) => {
                 </figure>
                 <div className="mt-4">
                     <div className='flex items-center'>
-                        <img className='w-5 h-5' src={userImg} alt="" /> <h2 className="card-title ml-2">Viral Kohli</h2>
+                        <img className='w-5 h-5' src={userImg} alt="" /> <h2 className="card-title ml-2 font-bold">Viral Kohli</h2>
                     </div>
                     <div className='flex items-center justify-between mt-2 pb-4 border-b border-gray-200'>
                         <div className='flex items-center'>
@@ -24,8 +24,18 @@ const AvailablePlayers = ({ playersPromise }) => {
                         </div>
                         <button className='btn btn-sm'>All-Rounder</button>
                     </div>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary mt-4">Buy Now</button>
+
+                    <div className='flex justify-between font-bold pt-3'>
+                        <span>Rating</span>
+                        <span>5</span>
+                    </div>
+                    <div className='flex justify-between  pt-2'>
+                        <span className='font-semibold'>Left hand bat</span>
+                        <span className='text-gray-500'>Right hand bat</span>
+                    </div>
+                    <div className="card-actions flex justify-between items-center mt-2">
+                        <p className='font-semibold'>Price: $150000</p>
+                        <button className="btn ">Choose Player</button>
                     </div>
                 </div>
             </div>
