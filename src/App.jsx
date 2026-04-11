6a724eeed3215556ec6c4b7e2099d02a7352264b
mahabubalam-gbs <mahabubalam.gbs@gmail.com>
2026-04-11 21:03:52 +0600

    Toggle functionality's basic design done

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,13 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <div className='max-w-300 mx-auto flex justify-between items-center py-5'>
+        <h1>Available Players</h1>
+        <div>
+          <button className='py-2 px-5 border border-r-0 rounded-l-2xl border-gray-400'>Available</button>
+          <button className='py-2 px-5 border border-l-0 rounded-r-2xl border-gray-400'>Selected</button>
+        </div>
+      </div>
       <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
         <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
       </Suspense>
